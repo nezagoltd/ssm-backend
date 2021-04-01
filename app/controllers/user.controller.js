70ed525f26c6312d990e16dec0868236178013f4
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-01 18:46:14 +0200

    Add controller with all 5 verbs

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,9 +1,23 @@
-import UserService from '../services/user.service';
-
 /**
  * @class
  */
-class UserController extends UserService {
+class UserController {
+  /**
+   * @constructor
+   * @param {object} service
+   */
+  constructor(service) {
+    this.service = service;
+  }
 
+  all = () => {}
+
+  create = () => {}
+
+  show = () => {}
+
+  update = () => {}
+
+  delete = () => {}
 }
-export default new UserController();
+export default UserController;
