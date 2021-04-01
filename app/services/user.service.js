3ed03259b6751f2f829e6ef9e547891c912ae581
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-01 16:20:07 +0200

    Fix the datacable

--- app/services/user.service.js
+++ app/services/user.service.js
@@ -1,4 +1,4 @@
-import ApplicationModel from '../models/applicationModel';
+import DataCable from '../../config/initializers/data.cable';
 import models from '../models';
 
 const { User, UserRole } = models;
@@ -6,7 +6,7 @@ const { User, UserRole } = models;
  * @description this class user service contains all methods regarding creating user
  * updating user, reading user and deleting a user
  */
-class UserService extends ApplicationModel {
+class UserService extends DataCable {
   /**
      * @description a constructor and a super methods to call parent class methods
      */
