d620603d4bd1504cec6afdc183878df9b7777915
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-02 21:22:50 +0200

    Move all routes to routers

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import apiRouter from './apis';
 import welcomeRoute from './welcome.route';
+import docRouter from './docRouter.route';
 
 const allRoutes = Router();
 
 allRoutes.use('/', welcomeRoute);
 allRoutes.use('/api', apiRouter);
+allRoutes.use('/public', docRouter);
 
 export default allRoutes;
