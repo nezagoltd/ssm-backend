e16e426d8a37a7e684380fcef9e3bc80e5e2036a
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-02 19:03:20 +0200

    Add response to usercontroller

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,22 +1,33 @@
+import services from '../services';
+
+const { UserServiceInstance } = services;
+
+/**
+ * @description it gets a request and then it extracts user parameters from that request
+ * @param {object} req
+ * @returns {object} userParams
+ */
+const getUserParams = req => {
+  const {
+    firstName, lastName, email, password,
+  } = req.body;
+
+  return {
+    firstName, lastName, email, password,
+  };
+};
+
 /**
  * @class
  */
 class UserController {
   /**
-   * @constructor
-   * @param {object} service
-   */
-  constructor(service) {
-    this.service = service;
-  }
-
-  /**
-   * @param {object} req
-   * @param {object} res
-   * @returns {void}
-   * @description GET: /users
-   */
-  all = (req, res) => {}
+  //  * @param {object} req
+  //  * @param {object} res
+  //  * @returns {void}
+  //  * @description GET: /users
+  //  */
+  // all = (req, res) => {}
 
   /**
    * @param {object} req
@@ -24,30 +35,39 @@ class UserController {
    * @returns {void}
    * @description POST: /users
    */
-  create = (req, res) => {}
+  create = async (req, res) => {
+    const dataToSave = getUserParams(req);
+    const savedUser = await UserServiceInstance.saveAll(dataToSave);
+    if (savedUser) {
+      // process a success response
+    } else {
+      // process a failure response
+    }
+  }
 
   /**
-   * @param {object} req
-   * @param {object} res
-   * @returns {void}
-   * @description GET: /users/userId
-   */
-  show = (req, res) => {}
+  //  * @param {object} req
+  //  * @param {object} res
+  //  * @returns {void}
+  //  * @description GET: /users/userId
+  //  */
+  // show = (req, res) => {}
 
   /**
-   * @param {object} req
-   * @param {object} res
-   * @returns {void}
-   * @description PATCH: /users/userId
-   */
-  update = (req, res) => {}
+  //  * @param {object} req
+  //  * @param {object} res
+  //  * @returns {void}
+  //  * @description PATCH: /users/userId
+  //  */
+  // update = (req, res) => {}
 
-  /**
-   * @param {object} req
-   * @param {object} res
-   * @returns {void}
-   * @description DELETE: /users/userId
-   */
-  delete = (req, res) => {}
+  // /**
+  //  * @param {object} req
+  //  * @param {object} res
+  //  * @returns {void}
+  //  * @description DELETE: /users/userId
+  //  */
+  // delete = (req, res) => {}
 }
+
 export default UserController;
