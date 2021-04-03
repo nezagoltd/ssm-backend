2f1f41daac9773b66d8f7ae6c19ab388e67f198d
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-03 14:41:42 +0200

    Add codes to send email

--- app/helpers/mailer.helper.js
+++ app/helpers/mailer.helper.js
@@ -4,43 +4,57 @@ import nodemailer from 'nodemailer';
 dotenv.config();
 
 /**
- * sendEmail
  * @param {Object} mailData - Email information
  * @returns {void}
  * @description it sends email to passed data
  */
 const sendEmail = async (mailData) => {
   const {
-    from, to, mailSubject, mailContent,
+    mailSentTo,
+    mailSubject,
+    verifyEmailContentPlainText: mailContentText,
+    verifyEmailContentHTML: mailContentHTML,
   } = mailData;
 
+  const { APPLICATION_EMAIL, APPLICATION_EMAIL_PASSWORD } = process.env;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: APPLICATION_EMAIL,
+      pass: APPLICATION_EMAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const mailConfigs = {
+    from: APPLICATION_EMAIL,
+    to: mailSentTo,
+    subject: mailSubject,
+    html: mailContentHTML,
+    text: mailContentText,
+  };
+
+  let isSent;
+
   try {
-    const transporter = nodemailer.createTransport({
-      host: 'smtp-relay.sendinblue.com',
-      port: 587,
-      auth: {
-        user: process.env.USER,
-        pass: process.env.PASS,
-      },
+    isSent = await transporter.sendMail(mailConfigs, (err, sendResult) => {
+      let result;
+      if (err) {
+        result = false;
+      } else if (sendResult) {
+        result = true;
+      }
+      return result;
     });
-
-    // send mail with defined transport object
-    const info = await transporter.sendMail({
-      from, // sender address
-      to, // list of receivers
-      subject, // Subject line
-      text, // plain text body
-      html: {
-        path: path.resolve(__dirname, '../template/mail.html'),
-      }, // html body
-    });
-
-    console.log(`Message sent: ${info.messageId}`);
-    return `Message sent: ${info.messageId}`;
   } catch (error) {
-    console.error(error);
     throw new Error(
       `Something went wrong in the sendmail method. Error: ${error.message}`,
     );
   }
+  return isSent;
 };
+
+export default sendEmail;
