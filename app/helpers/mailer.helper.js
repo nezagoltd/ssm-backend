b4e65f433df2287553c6f88676383908a957001e
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-03 13:36:40 +0200

    Add email content

--- app/helpers/mailer.helper.js
+++ app/helpers/mailer.helper.js
@@ -1,3 +1,46 @@
 import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
 
 dotenv.config();
+
+/**
+ * sendEmail
+ * @param {Object} mailData - Email information
+ * @returns {void}
+ * @description it sends email to passed data
+ */
+const sendEmail = async (mailData) => {
+  const {
+    from, to, mailSubject, mailContent,
+  } = mailData;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp-relay.sendinblue.com',
+      port: 587,
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+      },
+    });
+
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from, // sender address
+      to, // list of receivers
+      subject, // Subject line
+      text, // plain text body
+      html: {
+        path: path.resolve(__dirname, '../template/mail.html'),
+      }, // html body
+    });
+
+    console.log(`Message sent: ${info.messageId}`);
+    return `Message sent: ${info.messageId}`;
+  } catch (error) {
+    console.error(error);
+    throw new Error(
+      `Something went wrong in the sendmail method. Error: ${error.message}`,
+    );
+  }
+};
--- app/helpers/messages.helper.js
+++ app/helpers/messages.helper.js
@@ -1,6 +1,74 @@
 export const successMessages = {
-  accountCreatedTemporary: 'Congratulations!\n\nYour account has been created temporary.!\n\nWe have sent an email to the system administrator, please wait for them to confirm your account.',
+  accountCreatedTemporary: 'Your account has been created temporary!\n\nWe have sent an email to your email address you provided, kindly check out your email, and click on the link within that email to verify your email addrdress.!\n\nPlease note that, if this account is not verified for 7 days, it will be removed from the system, you will need to create a new one!',
+  approveEmailAddressToAdmin: 'Congratulations!\n\nYour account has been created temporary.!\n\nWe have sent an email to the system administrator, please wait for them to confirm your account.',
 };
 export const errorMessages = {
   accountFailedToCreate: 'Sorry!\nAccount was not created, something weird went wrong, please try again!',
+};
+
+/**
+ * @param {object} userData
+ * @param {object} token
+ * @param {object} appURL
+ * @returns {object} verifyEmailContent
+ * @description It returns the verify email address message content
+ */
+export const generateVerifyEmailContent = (userData, token, appURL) => {
+  const { firstName, lastName } = userData;
+  const verifyEmailContentHTML = `
+  <div style="width: 70%; margin: 0 auto; font-family: sans-serif;">
+    <nav style="background-color: #c4c4c4; padding: 0.1rem;">
+      <h1 style="text-align: center;">SSM needs you to verify your email address</h1>
+    </nav>
+
+    <div style="padding: 2rem;">
+      <h2>Hi ${firstName} ${lastName}!</h2>
+
+      <p>You recently registered on Social Saving Management (SSM), we sent you this email in order to verify if the email you provided us is viable,
+      for the sake of security and better services you receive from SSM!</p>
+
+      <p>Kindly click the button below, so that we will know that this email belongs to you. Keep in mind that you need to always remember your email
+      and password you used and don’t share it with anyone else!</p>
+
+      <div style="padding: 1rem 0 1rem 0;">
+        <a
+          href="${appURL}/users/verify-email?token=${token}"
+          style="background-color: #861A02; color: #ffffff; width: 40%; padding: 0.8rem; text-decoration: none; border-radius: 0.2rem;">
+          Verify email address</a>
+      </div>
+
+      <p>If you didn’t register on Social Saving Management system recently,
+      we are sorry for that inconvenience, ignore this email!</p>
+
+      <div>
+        <p>Yours truly,</p>
+        <p>Social Saving Management</p>
+      </div>
+    </div>
+    <footer style="background-color: #c4c4c4; padding: 0.1rem">
+      <p style="text-align: center; font-size: 0.8rem;">© 2021 <a href="">SSM</a>. All rights reserved.</p>
+    </footer>
+  </div>
+  `;
+
+  const verifyEmailContentPlainText = `
+    SSM needs you to verify your email address
+
+    Hello ${firstName} ${lastName}!
+
+    You recently registered on Social Saving Management (SSM), we sent you this email in order to verify if the email you provided us is viable,
+      for the sake of security and better services you receive from SSM!
+
+    Kindly click the link below, so that we will know that this email belongs to you. Keep in mind that you need to always remember your email
+      and password you used and don’t share it with anyone else!
+
+    ${appURL}/users/verify-email?token=${token}
+
+    If you didn’t register on Social Saving Management system recently, we are sorry for that inconvenience, ignore this email!
+
+    
+    Yours truly,
+    Social Saving Management
+  `;
+  return { verifyEmailContentHTML, verifyEmailContentPlainText };
 };
