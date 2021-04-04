438a3b5055f2df45156036304cb0b83a96e07f97
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-04 13:30:55 +0200

    Add userrole saving middleware

--- app/middlewares/approveUserRoute/approveUserData.middleware.js
+++ app/middlewares/approveUserRoute/approveUserData.middleware.js
@@ -1,3 +1,24 @@
+import services from '../../services';
+
+/**
+ * @description it prepares userrole data
+ * @param {object} req
+ * @returns {object} userRoleData
+ */
+const getRoleData = async req => {
+  const { RoleServiceInstance } = services;
+  let userRoleData;
+  if (req.body.roleId) {
+    userRoleData.passedRoleId = req.body.roleId;
+  } else {
+    const accountantRole = await RoleServiceInstance.getBy({ name: 'accountant' });
+    const roleId = accountantRole.id;
+    userRoleData.passedRoleId = roleId;
+  }
+  userRoleData.passedUserId = req.params.userId;
+  return userRoleData;
+};
+
 /**
  * @description it sets data for approving new user
  * @param {object} req
@@ -6,9 +27,12 @@
  * @returns {void}
  */
 const dataForApprovingUser = async (req, res, next) => {
+  const userRoleData = await getRoleData(req);
   const { userId } = req.params;
   const dataToUpdate = { isApproved: true, isActive: true };
   const whereCondition = { id: userId };
+  req.body.passedRoleId = userRoleData.passedRoleId;
+  req.body.passedUserId = userRoleData.passedUserId;
   req.dataToUpdate = dataToUpdate;
   req.whereCondition = whereCondition;
   next();
