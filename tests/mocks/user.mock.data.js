80455d5d5cc12837f4aaa14e5b96afaebd5632a3
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-05 17:08:22 +0200

    Add mockdata content

---
+++ tests/mocks/user.mock.data.js
@@ -0,0 +1,10 @@
+export const userRegMock = {
+  firstName: 'userFname',
+  lastName: 'userLname',
+  email: 'userFname@ssm.com',
+  password: 'userFname',
+};
+
+export const userRoleMock = {
+  roleId: 1,
+};
