938e6b307e4cc1ce0020e9928e69a9030e9b6a61
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-05 18:26:42 +0200

    Add registerUser.test file

---
+++ tests/registerUser/approveUser.test.js
@@ -0,0 +1,28 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../../bin/main';
+// import { userRegMock } from '../mocks/user.mock.data';
+import { successMessages } from '../../app/helpers/messages.helper';
+
+const { roleAssignedSuccess } = successMessages;
+
+chai.use(chaiHttp);
+
+describe('Test the approval feature', () => {
+  it('Will approve a user without sending any data', (done) => {
+    chai
+      .request(server)
+      .patch('/api/users/approve-user/1')
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('message');
+        expect(res.body).to.have.property('token');
+        expect(res.body).to.have.property('data');
+        expect(res.body.message).to.be.a('string');
+        expect(res.body.message).to.equal(roleAssignedSuccess);
+        expect(res.body.data).to.be.equal(null);
+        expect(res.body.token).to.equal(null);
+        done(err);
+      });
+  });
+});
