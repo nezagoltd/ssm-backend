517aeaabe0cdeb7640bb3a8560777d4610b8e524
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-05 20:01:04 +0200

    Divide mailer helper into two methods

--- app/helpers/mail/mailer.helper.js
+++ app/helpers/mail/mailer.helper.js
@@ -1,22 +1,43 @@
 /**
  * @description it prepares and generate the mail body
+ * @param {string} firstName
+ * @param {string} lastName
  * @param {string} verifyURL
  * @returns {string} mailBody
  */
-const generateVerifyEmailBody = verifyURL => `
+const generateVerifyEmailHTML = (firstName, lastName, verifyURL) => `
+<div style="width: 70%; margin: 0 auto; font-family: sans-serif;">
+  <nav style="background-color: #c4c4c4; padding: 0.1rem;">
+    <h1 style="text-align: center;">SSM needs you to verify your email address</h1>
+  </nav>
+
+  <div style="padding: 2rem;">
+    <h2>Hi ${firstName} ${lastName}!</h2>
+
+    
   <p>You recently registered on Social Saving Management (SSM), we sent you this email in order to verify if the email you provided us is viable,
       for the sake of security and better services you receive from SSM!</p>
 
   <p>Kindly click the button below, so that we will know that this email belongs to you. Keep in mind that you need to always remember your email
-  and password you used and don’t share it with anyone else!</p>
+    and password you used and don’t share it with anyone else!</p>
 
   <div style="padding: 1rem 0 1rem 0;">
-    <a href="${verifyURL}" style="background-color: #861A02; color: #ffffff; width: 40%; padding: 0.8rem; text-decoration: none; border-radius: 0.2rem;">
-      Verify email address</a>
+  <a href="${verifyURL}" style="background-color: #861A02; color: #ffffff; width: 40%; padding: 0.8rem; text-decoration: none; border-radius: 0.2rem;">
+    Verify email address</a>
   </div>
 
-  <p>If you didn’t register on Social Saving Management system recently,
-  we are sorry for that inconvenience, ignore this email!</p>
+    <p>If you didn’t register on Social Saving Management system recently,
+    we are sorry for that inconvenience, ignore this email!</p>
+
+    <div>
+      <p>Yours truly,</p>
+      <p>Social Saving Management</p>
+    </div>
+  </div>
+  <footer style="background-color: #c4c4c4; padding: 0.1rem">
+    <p style="text-align: center; font-size: 0.8rem;">© 2021 <a href="${appURL}">SSM</a>. All rights reserved.</p>
+  </footer>
+</div>
   `;
 
 /**
@@ -29,27 +50,7 @@ const generateVerifyEmailBody = verifyURL => `
 export const generateVerifyEmailContent = (userData, token, appURL) => {
   const { firstName, lastName } = userData;
   const verifyURL = `${appURL}/api/users/verify-email?token=${token}`;
-  const verifyEmailContentHTML = `
-  <div style="width: 70%; margin: 0 auto; font-family: sans-serif;">
-    <nav style="background-color: #c4c4c4; padding: 0.1rem;">
-      <h1 style="text-align: center;">SSM needs you to verify your email address</h1>
-    </nav>
-
-    <div style="padding: 2rem;">
-      <h2>Hi ${firstName} ${lastName}!</h2>
-
-      ${generateVerifyEmailBody(verifyURL)}
-
-      <div>
-        <p>Yours truly,</p>
-        <p>Social Saving Management</p>
-      </div>
-    </div>
-    <footer style="background-color: #c4c4c4; padding: 0.1rem">
-      <p style="text-align: center; font-size: 0.8rem;">© 2021 <a href="${appURL}">SSM</a>. All rights reserved.</p>
-    </footer>
-  </div>
-  `;
+  const verifyEmailContentHTML = generateVerifyEmailHTML(firstName, lastName, appURL);
 
   const verifyEmailContentPlainText = `
     Note: If you cannot see this email properly, please use a browser
