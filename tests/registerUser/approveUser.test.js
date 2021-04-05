e2f8411893cca3f60b10e569198fd08dfb87e1d6
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-05 18:35:06 +0200

    Test the negative case of approve

--- tests/registerUser/approveUser.test.js
+++ tests/registerUser/approveUser.test.js
@@ -1,7 +1,7 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../../bin/main';
-// import { userRegMock } from '../mocks/user.mock.data';
+import { userRoleMock } from '../mocks/user.mock.data';
 import { successMessages } from '../../app/helpers/messages.helper';
 
 const { roleAssignedSuccess } = successMessages;
@@ -13,6 +13,23 @@ describe('Test the approval feature', () => {
     chai
       .request(server)
       .patch('/api/users/approve-user/1')
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('message');
+        expect(res.body).to.have.property('token');
+        expect(res.body).to.have.property('data');
+        expect(res.body.message).to.be.a('string');
+        expect(res.body.message).to.equal(roleAssignedSuccess);
+        expect(res.body.data).to.be.equal(null);
+        expect(res.body.token).to.equal(null);
+        done(err);
+      });
+  });
+  it('Will approve a user with sending roleData data', (done) => {
+    chai
+      .request(server)
+      .patch('/api/users/approve-user/1')
+      .send(userRoleMock)
       .end((err, res) => {
         expect(res.body).to.be.an('object');
         expect(res.body).to.have.property('message');
