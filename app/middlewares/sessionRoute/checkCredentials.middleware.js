bf4fcc40bd29b558733919bc66e43692dcc651b0
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-07 08:02:25 +0200

    Create checkCredentials.middleware file

---
+++ app/middlewares/sessionRoute/checkCredentials.middleware.js
@@ -0,0 +1,12 @@
+/**
+ * @description it checks the credentials and allow to go to the next step if they are right
+ * @param {object} req
+ * @param {object} res
+ * @param {object} next
+ * @returns {void}
+ */
+const checkCredentials = (req, res, next) => {
+
+};
+
+export default checkCredentials;
