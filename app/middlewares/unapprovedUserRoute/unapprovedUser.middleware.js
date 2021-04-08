b8aa1d5cc6aef005595b4f69612497c7d0bcfa90
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-08 10:11:48 +0200

    Add unapproved tasks codes

---
+++ app/middlewares/unapprovedUserRoute/unapprovedUser.middleware.js
@@ -0,0 +1,12 @@
+/**
+ * @description it prepares the whereConditions and saves it in request
+ * @param {object} req
+ * @param {object} res
+ * @param {object} next
+ * @returns {void}
+ */
+const unapprovedUserData = (req, res, next) => {
+  const {  } = req.query;
+};
+
+export default unapprovedUserData;
