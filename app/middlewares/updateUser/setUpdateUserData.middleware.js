965b94d69eeb7fbf07d8ecc147cc5b0e2a642d6f
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-09 13:19:20 +0200

    Add middleware of updating user

---
+++ app/middlewares/updateUser/setUpdateUserData.middleware.js
@@ -0,0 +1,44 @@
+import services from '../../services';
+import { sendErrorResponse } from '../../helpers/response.helper';
+import { failureCodes } from '../../helpers/statusCodes.helper';
+import { errorMessages } from '../../helpers/messages.helper';
+
+const { notFound } = failureCodes;
+const { noRecordFound } = errorMessages;
+
+/**
+ * @description It checks if user exists
+ * @param {integer} id
+ * @param {object} res
+ * @returns {void}
+ */
+const isUserExists = async (id, res) => {
+  const { UserServiceInstance } = services;
+  const foundUser = await UserServiceInstance.getBy({ id });
+  if (foundUser) {
+    return true;
+  }
+  sendErrorResponse(res, notFound, noRecordFound);
+  return false;
+};
+
+/**
+ * @description it sets data for approving new user
+ * @param {object} req
+ * @param {object} res
+ * @param {object} next
+ * @returns {void}
+ */
+const dataForUpdatingUser = async (req, res, next) => {
+  const dataToUpdate = req.body;
+  const { userId } = req.params;
+  const isUserFound = await isUserExists(userId, res);
+  if (isUserFound) {
+    const whereCondition = { id: userId };
+    req.dataToUpdate = dataToUpdate;
+    req.whereCondition = whereCondition;
+    next();
+  }
+};
+
+export default dataForUpdatingUser;
