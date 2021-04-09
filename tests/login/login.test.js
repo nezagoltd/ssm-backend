86e45037e8d2bcc0bd359ee061d4ab27b06e15df
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-09 11:52:36 +0200

    Add logout tests

--- tests/login/login.test.js
+++ tests/login/login.test.js
@@ -3,11 +3,14 @@ import chaiHttp from 'chai-http';
 import server from '../../bin/main';
 import { userInvalidEmailLogin, userValidLoginData } from '../mocks/user.mock.data';
 import { errorMessages, successMessages } from '../../app/helpers/messages.helper';
+import { successCodes } from '../../app/helpers/statusCodes.helper';
 
 chai.use(chaiHttp);
 
 const { loginFail } = errorMessages;
-const { loginSuccess } = successMessages;
+const { loginSuccess, loggedOut } = successMessages;
+const { ok } = successCodes;
+let loginToken = '';
 
 describe('Test the login feature', () => {
   it('Will login successfully, and send token on a successful login', (done) => {
@@ -16,6 +19,8 @@ describe('Test the login feature', () => {
       .post('/api/session/login')
       .send(userValidLoginData)
       .end((err, res) => {
+        loginToken = res.body.token;
+        expect(res.status).to.equal(ok);
         expect(res.body).to.be.an('object');
         expect(res.body).to.have.property('message');
         expect(res.body).to.have.property('token');
@@ -37,6 +42,27 @@ describe('Test the login feature', () => {
         expect(res.body).to.have.property('error');
         expect(res.body.error).to.be.a('string');
         expect(res.body.error).to.equal(loginFail);
+        done(err);
+      });
+  });
+});
+
+describe('Test the logout feature', () => {
+  it('Will Logout a logged in user', (done) => {
+    chai
+      .request(server)
+      .delete('/api/session/logout')
+      .set('Authorization', `Bearer ${loginToken}`)
+      .end((err, res) => {
+        expect(res.status).to.equal(ok);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('data');
+        expect(res.body).to.have.property('token');
+        expect(res.body).to.have.property('message');
+        expect(res.body.message).to.be.a('string');
+        expect(res.body.data).to.equal(null);
+        expect(res.body.token).to.equal(null);
+        expect(res.body.message).to.equal(loggedOut);
         done(err);
       });
   });
