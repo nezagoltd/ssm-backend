30706204794f3242267325f452701254c37ea885
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-09 15:34:51 +0200

    Add send response middleware

---
+++ app/middlewares/updateUser/respondAfterUpdate.middleware.js
@@ -0,0 +1,26 @@
+import { sendSuccessResponse, sendErrorResponse } from '../../helpers/response.helper';
+import { failureCodes, successCodes } from '../../helpers/statusCodes.helper';
+import { successMessages, errorMessages } from '../../helpers/messages.helper';
+
+const { ok } = successCodes;
+const { notFound } = failureCodes;
+const { userFailedToUpdate } = errorMessages;
+const { updateSuccess } = successMessages;
+
+/**
+ * @description it sets data for approving new user
+ * @param {object} req
+ * @param {object} res
+ * @param {object} next
+ * @returns {void}
+ */
+const setDataForUpdatingUser = async (req, res) => {
+  const { statusCode } = req;
+  if (statusCode === ok) {
+    sendSuccessResponse(res, ok, updateSuccess, null, null);
+  } else {
+    sendErrorResponse(res, notFound, userFailedToUpdate);
+  }
+};
+
+export default setDataForUpdatingUser;
