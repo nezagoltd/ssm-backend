905e6419bb278c5bc623d324cc775e9158ba1bdb
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-10 15:49:11 +0200

    Add more precision on testing retrieve role route

--- tests/manageRoles/crudRoles.test.js
+++ tests/manageRoles/crudRoles.test.js
@@ -8,7 +8,7 @@ import { failureCodes, successCodes } from '../../app/helpers/statusCodes.helper
 chai.use(chaiHttp);
 
 const { roleCreateFail } = errorMessages;
-const { roleCreateSuccess } = successMessages;
+const { roleCreateSuccess, recordFound } = successMessages;
 const { internalServerError } = failureCodes;
 const { ok, created } = successCodes;
 
@@ -28,6 +28,30 @@ describe('Test the manage role feature', () => {
         expect(res.body.token).to.equal(null);
         expect(res.body.data).to.be.an('object');
         expect(res.body.message).to.equal(roleCreateSuccess);
+        done(err);
+      });
+  });
+  it('Will read all roles successfully', (done) => {
+    chai
+      .request(server)
+      .get('/api/roles')
+      .end((err, res) => {
+        console.log({ sentData: res.body });
+        expect(res.status).to.equal(ok);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('message');
+        expect(res.body).to.have.property('token');
+        expect(res.body).to.have.property('data');
+        expect(res.body.message).to.be.a('string');
+        expect(res.body.token).to.equal(null);
+        expect(res.body.data).to.be.an('object');
+        expect(res.body.data).to.have.property('count');
+        expect(res.body.data).to.have.property('rows');
+        expect(res.body.data.count).to.be.a('number');
+        expect(res.body.data.count).to.be.greaterThan(0);
+        expect(res.body.data.rows).to.be.an('array');
+        expect(res.body.data.rows.length).to.be.greaterThan(0);
+        expect(res.body.message).to.equal(recordFound);
         done(err);
       });
   });
