dbcaf5a31f7e8a13c4b66746e9d5a3d0952e445f
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-10 12:23:59 +0200

    Add roleRouter file

--- config/routes/apis/index.js
+++ config/routes/apis/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userRouter from './user.routes';
 import sessionRouter from './login.routes';
+import roleRouter from './role.routes';
 
 const apiRouter = Router();
 
 apiRouter.use('/users', userRouter);
 apiRouter.use('/session', sessionRouter);
+apiRouter.use('/roles', roleRouter);
 
 export default apiRouter;
---
+++ config/routes/apis/role.routes.js
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const roleRouter = Router();
+
+export default roleRouter;
