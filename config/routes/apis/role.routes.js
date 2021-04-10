a47dd437a19542d681d4e76658b96e6d8e41a5d5
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-10 13:32:30 +0200

    Make createRole route work successfully

--- config/routes/apis/role.routes.js
+++ config/routes/apis/role.routes.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import controllers from '../../../app/controllers';
 
 const roleRouter = Router();
+const { RoleControllerInstance } = controllers;
+
+roleRouter.post('/create', RoleControllerInstance.create);
 
 export default roleRouter;
