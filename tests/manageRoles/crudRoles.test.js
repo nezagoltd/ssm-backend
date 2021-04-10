cdd5641ffe84c7f64c8381b80bff36d2a5742245
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-10 15:41:13 +0200

    Add test for create role feature

--- tests/manageRoles/crudRoles.test.js
+++ tests/manageRoles/crudRoles.test.js
@@ -1,34 +1,33 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../../bin/main';
-import { userValidLoginData } from '../mocks/user.mock.data';
+import { validRoleData } from '../mocks/role.mock.data';
 import { errorMessages, successMessages } from '../../app/helpers/messages.helper';
-import { successCodes } from '../../app/helpers/statusCodes.helper';
+import { failureCodes, successCodes } from '../../app/helpers/statusCodes.helper';
 
 chai.use(chaiHttp);
 
-const { loginFail } = errorMessages;
-const { loginSuccess, loggedOut } = successMessages;
-const { ok } = successCodes;
-let loginToken = '';
+const { roleCreateFail } = errorMessages;
+const { roleCreateSuccess } = successMessages;
+const { internalServerError } = failureCodes;
+const { ok, created } = successCodes;
 
-describe('Test the login feature', () => {
-  it('Will login successfully, and send token on a successful login', (done) => {
+describe('Test the manage role feature', () => {
+  it('Will create role successfully', (done) => {
     chai
       .request(server)
-      .post('/api/session/login')
-      .send(userValidLoginData)
+      .post('/api/roles/create')
+      .send(validRoleData)
       .end((err, res) => {
-        loginToken = res.body.token;
-        expect(res.status).to.equal(ok);
+        expect(res.status).to.equal(created);
         expect(res.body).to.be.an('object');
         expect(res.body).to.have.property('message');
         expect(res.body).to.have.property('token');
         expect(res.body).to.have.property('data');
         expect(res.body.message).to.be.a('string');
-        expect(res.body.token).to.be.a('string');
-        expect(res.body.data).to.equal(null);
-        expect(res.body.message).to.equal(loginSuccess);
+        expect(res.body.token).to.equal(null);
+        expect(res.body.data).to.be.an('object');
+        expect(res.body.message).to.equal(roleCreateSuccess);
         done(err);
       });
   });
