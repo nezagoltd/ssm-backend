e3de171e0634670a4469f3e403ff591711a144c1
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-10 15:13:22 +0200

    Add route for create

--- config/routes/apis/role.routes.js
+++ config/routes/apis/role.routes.js
@@ -7,5 +7,6 @@ const { RoleControllerInstance } = controllers;
 roleRouter.post('/create', RoleControllerInstance.create);
 roleRouter.get('/', RoleControllerInstance.all);
 roleRouter.get('/all/:roleId', RoleControllerInstance.show);
+roleRouter.patch('/update/:roleId', RoleControllerInstance.update);
 
 export default roleRouter;
