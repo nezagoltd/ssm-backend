f00589630a986d67ffb725be46b8c6938f686ca5
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-11 10:55:11 +0200

    Refactor interest rate and credit type into interest rate

---
+++ app/models/interestrate.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class interestRate extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  interestRate.init({
+    creditType: DataTypes.STRING,
+    rate: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'interestRate',
+  });
+  return interestRate;
+};
---
+++ db/migrate/20210411085252-create-interest-rate.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('interestRates', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      creditType: {
+        type: Sequelize.STRING
+      },
+      rate: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('interestRates');
+  }
+};
