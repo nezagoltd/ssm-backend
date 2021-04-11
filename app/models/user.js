447d929da3c2334b2a4c4341773b96f70c9daa9d
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-11 12:01:32 +0200

    Fix associatiaon

--- app/models/user.js
+++ app/models/user.js
@@ -16,7 +16,7 @@ export default (sequelize, DataTypes) => {
         onDelete: 'CASCADE',
         onUpdate: 'CASCADE',
       });
-      User.hasMany(models.UserRole, {
+      User.hasMany(models.InterestRate, {
         foreignKey: 'creatorId',
         onDelete: 'CASCADE',
         onUpdate: 'CASCADE',
