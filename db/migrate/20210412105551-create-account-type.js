246fd230579ebfd2ab8c2d7534db875c9ae84a91
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-12 12:58:08 +0200

    ft: Add manage account type feature
    - Generate model and migration
    
    [Finishes#177515938]

---
+++ db/migrate/20210412105551-create-account-type.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('AccountTypes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      creatorId: {
+        type: Sequelize.INTEGER
+      },
+      typeName: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('AccountTypes');
+  }
+};
