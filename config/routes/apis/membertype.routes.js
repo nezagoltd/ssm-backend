308d0f111b7fb2676c0cd15e1ef2a272a9be22e1
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-12 23:08:19 +0200

    Customize meber type routes

--- config/routes/apis/membertype.routes.js
+++ config/routes/apis/membertype.routes.js
@@ -3,12 +3,12 @@ import controllers from '../../../app/controllers';
 
 const memberTypeRouter = Router();
 
-const { AccountTypeControllerInstance } = controllers;
+const { MemberTypeControllerInstance } = controllers;
 
-memberTypeRouter.get('/', AccountTypeControllerInstance.all);
-memberTypeRouter.post('/create', AccountTypeControllerInstance.create);
-memberTypeRouter.get('/all/:accountTypeId', AccountTypeControllerInstance.show);
-memberTypeRouter.patch('/update/:accountTypeId', AccountTypeControllerInstance.update);
-memberTypeRouter.delete('/:accountTypeId', AccountTypeControllerInstance.delete);
+memberTypeRouter.get('/', MemberTypeControllerInstance.all);
+memberTypeRouter.post('/create', MemberTypeControllerInstance.create);
+memberTypeRouter.get('/all/:memberTypeId', MemberTypeControllerInstance.show);
+memberTypeRouter.patch('/update/:memberTypeId', MemberTypeControllerInstance.update);
+memberTypeRouter.delete('/:memberTypeId', MemberTypeControllerInstance.delete);
 
 export default memberTypeRouter;
